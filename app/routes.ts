ab1b373c407e4cff54ab42a39583221487c12b84
tswlun002 <1998tsewu@gmail.com>
2026-01-27 23:31:13 +0200

    Added route design of the application

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,26 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {type RouteConfig, index, route, layout, type RouteConfigEntry} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+
+const authLayoutPages: RouteConfigEntry[] = [
+
+    index("pages/home.tsx"),
+    route("branches/:branchId/slots", "pages/branch-slots.tsx"),
+    route("register", "pages/auth/register.tsx"),
+    route("register/email-verification", "pages/auth/email-verification.tsx"),
+    route("login", "pages/auth/login.tsx"),
+    route("login/password", "pages/auth/forgot-password.tsx"),
+    route("login/password/reset", "pages/auth/reset-password.tsx"),
+
+    layout("layouts/appointment-layout.tsx", [
+        route("appointments", "pages/appointment/appointments.tsx"),
+        route("appointments/create", "pages/appointment/create-appointment.tsx"),
+        route("appointments/:appointmentId", "pages/appointment/appointment.tsx"),
+
+    ])
+]
+
+
+export default [
+    layout("layouts/auth-layout.tsx", authLayoutPages),
+    route("*", "pages/page-not-found.tsx")
+] satisfies RouteConfig;
