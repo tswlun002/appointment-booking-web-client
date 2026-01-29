8eb80289a07837cf902d4b5da0edaa084d260961
tswlun002 <1998tsewu@gmail.com>
2026-01-29 10:06:22 +0200

    Added  global style for h1 and added more capitec colors

--- app/resources/colors/colors.ts
+++ app/resources/colors/colors.ts
@@ -1,3 +1,5 @@
 export const white_background = '#FFFFFF';
 export const white_border = '#CCC'
 export const text_primary = '#3A3A3A'
+export const blue_primary='#2F70EF'
+export const blue_secondary='#0033a0'
