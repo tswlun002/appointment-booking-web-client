8d92ea50ac3c682c52a2f27ab65c8138caba2269
Lunga Tsewu <1998tsewu@gmail.com>
2026-01-29 09:49:18 +0200

    Added Branch-locator page

--- app/layouts/auth-layout.tsx
+++ app/layouts/auth-layout.tsx
@@ -1,6 +1,6 @@
 import {Outlet} from "react-router";
 
-import {white_background} from "~/resourses/colors/colors";
+import {white_background} from "~/resources/colors/colors";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "~/lib/react-query/Client";
 
