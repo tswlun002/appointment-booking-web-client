3662eb60100c312206f0d0706388e385297a3009
tswlun002 <1998tsewu@gmail.com>
2026-01-31 23:47:58 +0200

    dumpy branch data for test for now

--- app/pages/branch/Branches.tsx
+++ app/pages/branch/Branches.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import type {BranchLocation} from "~/domain/branch-locator/generated/model";
 
+type InitialState = BranchLocation[]
+const initialState:InitialState = []
 function Branches() {
+     let [state, set] = useState(initialState);
+     useEffect(() => {
+         fetch('http://127.0.0.1:57610/api/v1/branches/search', {method: 'GET'})
+             .then(res => res.json())
+             .then(data => set(initialState=>[...initialState, ...data]))
+             .catch(err => console.log(err));
+     }, [])
+
     return (
 
         <div >
-            <ul>
-                <li>Branch-1</li>
-                <li>Branch-1</li>
-                <li>Branch-1</li>
-                <li>Branch-1</li>
-                <li>Branch-1</li>
-                <li>Branch-1</li>
-                <li>Branch-1</li>
+            <ul>{
+                state.map((branch, idx)=><li className="p-5" key={idx}>{JSON.stringify(branch)}</li>)
+            }
             </ul>
         </div>
     );
