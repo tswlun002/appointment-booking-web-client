3137fb1118cbc36827953576f2b5a5748e551345
tswlun002 <1998tsewu@gmail.com>
2026-02-01 14:45:01 +0200

    Updated routes, add login as entry point(as index) and add branches under secured(RBAC) layout

--- app/routes.ts
+++ app/routes.ts
@@ -3,14 +3,12 @@ import {type RouteConfig, index, route, layout, type RouteConfigEntry} from "@re
 
 const authLayoutPages: RouteConfigEntry[] = [
 
-    index("pages/auth/register.tsx"),
+    index("pages/auth/login.tsx"),
     // route("branches/:branchId/slots", "pages/branch-slots.tsx"),
-    // route("register", "pages/auth/register.tsx"),
+    route("register", "pages/auth/register.tsx"),
     route("register/email-verification", "pages/auth/email-verification.tsx"),
-    // route("login", "pages/auth/login.tsx"),
     // route("login/password", "pages/auth/forgot-password.tsx"),
-    // route("login/password/reset", "pages/auth/reset-password.tsx"),
-
+    // route("login/password/reset", "pages/auth/reset-password.tsx")
     // layout("layouts/appointment-layout.tsx", [
     //     route("appointments", "pages/appointment/appointments.tsx"),
     //     route("appointments/create", "pages/appointment/create-appointment.tsx"),
@@ -19,8 +17,20 @@ const authLayoutPages: RouteConfigEntry[] = [
     // ])
 ]
 
+const securedLayoutPages: RouteConfigEntry[] = [
+    //layout("layout/branch-layout.tsx", [
+        route("branches","pages/branch/branches.tsx"),
+    //]),
+    // layout("layouts/appointment-layout.tsx", [
+    //     route("appointments", "pages/appointment/appointments.tsx"),
+    //     route("appointments/create", "pages/appointment/create-appointment.tsx"),
+    //     route("appointments/:appointmentId", "pages/appointment/appointment.tsx"),
+    //
+    // ])
+]
 
 export default [
     layout("layouts/auth-layout.tsx", authLayoutPages),
+    layout("layouts/secure-layout.tsx", securedLayoutPages),
     route("*", "pages/page-not-found.tsx")
 ] satisfies RouteConfig;
