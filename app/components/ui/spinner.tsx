10285a9726e810c64cd64622d8b042e749c7319b
Lunga Tsewu <1998tsewu@gmail.com>
2026-02-03 11:04:16 +0200

    Updated it to use centralized color

--- app/components/ui/spinner.tsx
+++ app/components/ui/spinner.tsx
@@ -1,11 +1,14 @@
+import { colors } from "~/resources/colors/colors";
+
 export function Spinner() {
     return (
         <div className="flex items-center justify-center">
             <div
-                className="inline-block h-5 w-5 mx-1 animate-spin rounded-full border-4 border-solid border-gray-50 border-t-transparent"
+                className="inline-block h-5 w-5 mx-1 animate-spin rounded-full border-4 border-solid border-t-transparent"
+                style={{ borderColor: colors.primary, borderTopColor: 'transparent' }}
                 role="status"
             >
-                <span className="sr-only">Loading...</span>
+                <span className="sr-only" style={{ color: colors.white }}>Loading...</span>
             </div>
         </div>
     );
