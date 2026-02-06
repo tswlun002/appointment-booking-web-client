19158efe2ff4aad4221bfde49aff91afdbfb5e24
tswlun002 <1998tsewu@gmail.com>
2026-02-06 10:46:27 +0200

    Added day of the week and Local date object

--- app/utils/CompanionObjects.ts
+++ app/utils/CompanionObjects.ts
@@ -1,3 +1,4 @@
+import {date} from "zod";
 
 
 export  function isNonNull<T>(obj: T | null | undefined):boolean{
@@ -32,3 +33,42 @@ export const TOKEN_EXPIRED_MESSAGE = "The access token expired";
 export enum PasswordVisibility  {
     "TOGGLE_VISIBILITY",
 "ALWAYS_VISIBLE","ALWAYS_HIDDEN"}
+
+
+export const DayOfWeek =  [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
+
+const options = {timeZone: 'Africa/Johannesburg'};
+const locale = "en-ZA";
+const saDate = new Date().toLocaleString('en-ZA', options);
+
+export const LocalDate ={
+    get now() { return new Date(new Date().toLocaleString(locale, options)); },
+    month: new Intl.DateTimeFormat(locale, {month: 'long'}).format(new Date(saDate)),
+    /**
+     * format date to YY-MM-DD
+     * @param date to format to YY-MM-DD
+     */
+    formattedDate: (date: Date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    },
+     dayName (date:Date){return  new Intl.DateTimeFormat(locale, { weekday: 'long' }).format(date);},
+     shortDayName(date:Date) {return  new Intl.DateTimeFormat(locale, { weekday: 'short' }).format(date);},
+     dayOfTheMonth(date:Date) {return date.getDate().toString().padStart(2, '0')},
+     formattedTime(date:Date){ return new Intl.DateTimeFormat(locale, {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true // Set to true for AM/PM
+    }).format(date)}
+}
